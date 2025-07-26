a7e828400558909d6e4ef915f7ec36aa718ae787
hwan0806 <rlfghks527@naver.com>
2025-07-26 21:44:02 +0900

    [refactor] cpg extension for linux env

--- example/cpg/cpg_conversion.hpp
+++ example/cpg/cpg_conversion.hpp
@@ -37,8 +37,15 @@
 #include <iostream>
 #include <string>
 #include <type_traits>
-#include <windows.h>
 
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+// Linux/Unix 환경에서 Windows 상수들을 정의
+#define CP_UTF8 65001
+#define CP_ACP 0
+#endif
 
 /**
  * @brief Includes subnamespace conversion.
@@ -57,6 +64,7 @@ namespace conversion {
  * href="002-conversion_8cpp_source.html">002-conversion.cpp</a>
  */
 inline void load_default_locale(bool ShowLocaleName = false) {
+#ifdef _WIN32
   // GetSystemDefaultLocaleName function :
   // https://goo.gl/WLLSG3
 
@@ -80,6 +88,12 @@ inline void load_default_locale(bool ShowLocaleName = false) {
 
   if (ShowLocaleName)
     std::wcout << L"locale name: " << locale_name << std::endl;
+#else
+  // Linux/Unix 환경에서는 기본 로케일 설정
+  setlocale(LC_ALL, "");
+  if (ShowLocaleName)
+    std::cout << "locale: " << setlocale(LC_ALL, nullptr) << std::endl;
+#endif
 }
 
 /**
@@ -100,6 +114,7 @@ inline std::string wstring_to_string(const std::wstring &wstr,
   if (wstr.empty())
     return "";
 
+#ifdef _WIN32
   // https://goo.gl/acoQBt
   int str_len =
       WideCharToMultiByte(codepage,
@@ -121,6 +136,20 @@ inline std::string wstring_to_string(const std::wstring &wstr,
                           str_len, NULL, NULL);
 
   return (converted == 0 ? "" : str);
+#else
+  // Linux/Unix 환경에서는 wcstombs 사용
+  std::string str;
+  str.reserve(wstr.size() * 4); // UTF-8은 최대 4바이트
+
+  for (wchar_t wc : wstr) {
+    char mb[MB_CUR_MAX];
+    int len = wctomb(mb, wc);
+    if (len > 0) {
+      str.append(mb, len);
+    }
+  }
+  return str;
+#endif
 }
 
 /** @example 002-conversion.cpp
@@ -130,171 +159,139 @@ inline std::string wstring_to_string(const std::wstring &wstr,
  */
 
 /**
- * @brief Converts std::string to std::wstring using codepage
+ * @brief Converts from std::string to std::wstring with codepage
  *
  * If fails, returns empty string, std::wstring(L"")
  *
- * @param str for string to convert
- * @param codepage for Code Page (default CP_UTF8)
- * @return std::wstring for the converted UTF16 string
+ * @param str for std::string to convert
+ * @param codepage for Code Page, default = CP_UTF8
+ * @return std::wstring for the converted string
  *
  * @sa <a target ="_blank"
  * href="002-conversion_8cpp_source.html">002-conversion.cpp</a>
- * @include 002-conversion.cpp
+ *
  */
 inline std::wstring string_to_wstring(const std::string &str,
                                       unsigned int codepage = CP_UTF8) {
   if (str.empty())
     return L"";
 
-  // https://goo.gl/upmy99
-  // Compute the character length of the string
-  int wstr_len =
-      MultiByteToWideChar(codepage, 0, str.c_str(), (int)str.size(), NULL, 0);
+#ifdef _WIN32
+  // https://goo.gl/acoQBt
+  int wstr_len = MultiByteToWideChar(codepage,
+                                     0, // dwFlag
+                                     str.c_str(), (int)str.size(), NULL, 0);
 
-  // if failed to compute the character length
+  // if failed to compute the byte count of the string
   if (wstr_len == 0)
     return L"";
 
-  // prepare buffer to hold converted string
-  // do not +1 to utf16_len, because std::wstring
-  // manages terminating null internally
+  // prepare a wstring buffer to
+  // hold the converted string
+  // do not +1 to wstr_len,
+  // because std::wstring manages terminating null
   std::wstring wstr(wstr_len, L'\0');
 
   int converted = MultiByteToWideChar(codepage, 0, str.c_str(), (int)str.size(),
                                       &wstr[0], wstr_len);
 
   return (converted == 0 ? L"" : wstr);
+#else
+  // Linux/Unix 환경에서는 mbstowcs 사용
+  std::wstring wstr;
+  wstr.reserve(str.size());
+
+  const char *mbstr = str.c_str();
+  size_t len = mbstowcs(nullptr, mbstr, 0);
+  if (len == static_cast<size_t>(-1))
+    return L"";
+
+  wstr.resize(len);
+  mbstowcs(&wstr[0], mbstr, len);
+  return wstr;
+#endif
 }
 
 /**
- * @brief Converts UTF16 std::wstring to UTF8 std::string
+ * @brief Converts from UTF-16 to UTF-8
  *
- * If fails, returns empty string, std::string("")
+ * @param utf16str for UTF-16 string to convert
+ * @return std::string for the converted UTF-8 string
  *
- * @param utf16str for UTF16 std::wstring to convert
- * @return std::string for the converted UTF8 string
- *
- * @sa <a target ="_blank"
- * href="002-conversion_8cpp_source.html">002-conversion.cpp</a>
  */
 inline std::string utf16_to_utf8(const std::wstring &utf16str) {
-  if (utf16str.empty())
-    return "";
   return wstring_to_string(utf16str, CP_UTF8);
 }
 
 /**
- * @brief Converts UTF8 std::string to UTF16 std::wstring
+ * @brief Converts from UTF-8 to UTF-16
  *
- * If fails, returns empty string, std::wstring(L"")
+ * @param utf8str for UTF-8 string to convert
+ * @return std::wstring for the converted UTF-16 string
  *
- * @param utf8str for UTF8 std::string to convert
- * @return std::wstring for the converted UTF16 string
- *
- * @sa <a target ="_blank"
- * href="002-conversion_8cpp_source.html">002-conversion.cpp</a>
  */
 inline std::wstring utf8_to_utf16(const std::string &utf8str) {
-  if (utf8str.empty())
-    return L"";
   return string_to_wstring(utf8str, CP_UTF8);
 }
 
 /**
- * @brief Converts UTF16 (const wchar_t*) to UTF8 (std::string)
+ * @brief Converts from UTF-16 to UTF-8
  *
- * This function converts zero terminated UTF16 or const wchar_t*
- * to UTF8 std::string. If conversion fails, returns empty string "".
+ * @param utf16str for UTF-16 string to convert
+ * @return std::string for the converted UTF-8 string
  *
- * @param utf16str for UTF16 string to convert to UTF8
- * @return std::string for UTF8 string, if fails returns empty string ""
- *
- * @sa <a target ="_blank"
- * href="002-conversion_8cpp_source.html">002-conversion.cpp</a>
  */
 inline std::string utf16_to_utf8(const wchar_t *utf16str) {
   if (utf16str == nullptr)
     return "";
-
-  // count of character of UTF16 string
-  int utf16_len = (int)wcslen(utf16str);
-  if (utf16_len < 1)
-    return "";
-
-  return utf16_to_utf8(std::wstring(utf16str));
+  return wstring_to_string(std::wstring(utf16str), CP_UTF8);
 }
 
 /**
- * @brief Converts UTF8 string (const char*) to UTF16 (std::wstring)
+ * @brief Converts from UTF-8 to UTF-16
  *
- * If fails, returns empty string or L"".
+ * @param utf8str for UTF-8 string to convert
+ * @return std::wstring for the converted UTF-16 string
  *
- * @param utf8str for UTF8 (const char*) to convert
- * @return std::wstring for the converted UTF16 (std::wstring)
- *
- * @sa <a target ="_blank"
- * href="002-conversion_8cpp_source.html">002-conversion.cpp</a>
  */
 inline std::wstring utf8_to_utf16(const char *utf8str) {
-  // if utf8str is nullptr, return empty string L""
   if (utf8str == nullptr)
     return L"";
-
-  // count of byte of the string to be converted
-  int utf8str_len = (int)strlen(utf8str);
-  if (utf8str_len < 1)
-    return L"";
-
-  return utf8_to_utf16(std::string(utf8str));
+  return string_to_wstring(std::string(utf8str), CP_UTF8);
 }
 
 /**
- * @brief Converts UTF16 wchar_t to UFT8 char.
+ * @brief Converts from UTF-16 character to UTF-8 character
  *
- * If fails, returns '\0'
+ * @param utf16_char for UTF-16 character to convert
+ * @return char for the converted UTF-8 character
  *
- * @param utf16_char for UTF16 wchar_t to convert
- * @return char for the converted UTF8 char
- *
- * @sa <a target ="_blank"
- * href="002-conversion_8cpp_source.html">002-conversion.cpp</a>
  */
 inline char utf16_to_utf8(const wchar_t utf16_char) {
-  wchar_t src_str[] = {utf16_char, L'\0'};
-  auto rlt = utf16_to_utf8(std::wstring(src_str));
-  return (rlt.empty() ? '\0' : rlt[0]);
+  std::wstring wstr(1, utf16_char);
+  std::string str = wstring_to_string(wstr, CP_UTF8);
+  return str.empty() ? '\0' : str[0];
 }
 
 /**
- * @brief Converts UTF8 char to UTF16 char.
+ * @brief Converts from UTF-8 character to UTF-16 character
  *
- * If fails, returns L'\0'
+ * @param utf8_char for UTF-8 character to convert
+ * @return wchar_t for the converted UTF-16 character
  *
- * @param utf8_char for UTF8 char to convert
- * @return wchar_t for the converted UTF16 character
- *
- * @sa <a target ="_blank"
- * href="002-conversion_8cpp_source.html">002-conversion.cpp</a>
  */
 inline wchar_t utf8_to_utf16(const char utf8_char) {
-  char src_str[] = {utf8_char, '\0'};
-  auto rlt = utf8_to_utf16(std::string(src_str));
-  return (rlt.empty() ? L'\0' : rlt[0]);
+  std::string str(1, utf8_char);
+  std::wstring wstr = string_to_wstring(str, CP_UTF8);
+  return wstr.empty() ? L'\0' : wstr[0];
 }
 
-/////////////////////////////////////////////////
-
 /**
- * @brief Converts Windows codepage std::string to utf16 std::wstring
+ * @brief Converts from Windows codepage to UTF-16
  *
- * If fails, returns empty string, std::wstring(L"")
+ * @param codepage_string for Windows codepage string to convert
+ * @return std::wstring for the converted UTF-16 string
  *
- * @param codepage_string for Windows codepage std::string to convert
- * @return std::wstring for the converted UTF16 std::wstring
- *
- * @sa <a target ="_blank"
- * href="002-conversion_8cpp_source.html">002-conversion.cpp</a>
  */
 inline std::wstring
 windows_codepage_to_utf16(const std::string &codepage_string) {
@@ -302,165 +299,140 @@ windows_codepage_to_utf16(const std::string &codepage_string) {
 }
 
 /**
- * @brief Converts UTF16 std::wstring to Windows codepage std::string
+ * @brief Converts from UTF-16 to Windows codepage
  *
- * If fails, returns empty string, std::string("")
- * @param utf16_string for UTF16 std::wstring to convert
+ * @param utf16_string for UTF-16 string to convert
  * @return std::string for the converted Windows codepage string
  *
- * @sa <a target ="_blank"
- * href="002-conversion_8cpp_source.html">002-conversion.cpp</a>
  */
 inline std::string utf16_to_windows_codepage(const std::wstring &utf16_string) {
   return wstring_to_string(utf16_string, CP_ACP);
 }
 
 /**
- * @brief Converts Windows codepage const char* to UTF16 std::wstring
+ * @brief Converts from Windows codepage to UTF-16
  *
- * If fails, returns empty string, std::wstring(L"")
- * @param codepage_string for Windows codepage const char* to convert
- * @return std::wstring for the converted UTF16 string
+ * @param codepage_string for Windows codepage string to convert
+ * @return std::wstring for the converted UTF-16 string
  *
- * @sa <a target ="_blank"
- * href="002-conversion_8cpp_source.html">002-conversion.cpp</a>
  */
 inline std::wstring windows_codepage_to_utf16(const char *codepage_string) {
   if (codepage_string == nullptr)
     return L"";
-  if (strlen(codepage_string) < 1)
-    return L"";
-
   return string_to_wstring(std::string(codepage_string), CP_ACP);
 }
 
 /**
- * @brief Converts UTF16 const wchar_t* to Windows codepage std::string
+ * @brief Converts from UTF-16 to Windows codepage
  *
- * If fails, returns empty string, std::string("")
- * @param utf16_string for UTF16 const wchar_t* to convert
+ * @param utf16_string for UTF-16 string to convert
  * @return std::string for the converted Windows codepage string
  *
- * @sa <a target ="_blank"
- * href="002-conversion_8cpp_source.html">002-conversion.cpp</a>
  */
 inline std::string utf16_to_windows_codepage(const wchar_t *utf16_string) {
   if (utf16_string == nullptr)
     return "";
-  if (wcslen(utf16_string) < 1)
-    return "";
-
   return wstring_to_string(std::wstring(utf16_string), CP_ACP);
 }
 
 /**
- * @brief Converts Windows codepage std::string to UTF8 std::string
+ * @brief Converts from Windows codepage to UTF-8
  *
- * If fails, returns empty string, std::string("")
+ * @param codepage_string for Windows codepage string to convert
+ * @return std::string for the converted UTF-8 string
  *
- * @param codepage_string for Windows codepage std::string to convert
- * @return std::string for the converted UTF8 string
- *
- * @sa <a target ="_blank"
- * href="002-conversion_8cpp_source.html">002-conversion.cpp</a>
  */
 inline std::string
 windows_codepage_to_utf8(const std::string &codepage_string) {
-  auto utf16_str = windows_codepage_to_utf16(codepage_string);
-
-  return (utf16_str.empty() ? "" : utf16_to_utf8(utf16_str));
+  std::wstring utf16 = windows_codepage_to_utf16(codepage_string);
+  return utf16_to_utf8(utf16);
 }
 
 /**
- * @brief Converts Windows codepage const char* to UTF8 std::string
+ * @brief Converts from Windows codepage to UTF-8
  *
- * If fails, return empty string, std::string("")
+ * @param codepage_string for Windows codepage string to convert
+ * @return std::string for the converted UTF-8 string
  *
- * @param codepage_string for Windows codepage const char* to convert
- * @return std::string for the converted UTF8 string
- *
- * @sa <a target ="_blank"
- * href="002-conversion_8cpp_source.html">002-conversion.cpp</a>
  */
 inline std::string windows_codepage_to_utf8(const char *codepage_string) {
   if (codepage_string == nullptr)
     return "";
-  if (strlen(codepage_string) < 1)
-    return "";
-
-  return windows_codepage_to_utf8(std::string(codepage_string));
+  std::wstring utf16 = windows_codepage_to_utf16(codepage_string);
+  return utf16_to_utf8(utf16);
 }
 
 /**
- * @brief Converts UTF8 std::string to Windows codepage string
+ * @brief Converts from UTF-8 to Windows codepage
  *
- * If fails, returns empty string, std::string("")
- *
- * @param utf8_string for UTF8 std::string to convert
+ * @param utf8_string for UTF-8 string to convert
  * @return std::string for the converted Windows codepage string
  *
- * @sa <a target ="_blank"
- * href="002-conversion_8cpp_source.html">002-conversion.cpp</a>
  */
 inline std::string utf8_to_windows_codepage(const std::string &utf8_string) {
-  auto utf16_str = utf8_to_utf16(utf8_string);
-
-  return (utf16_str.empty() ? "" : utf16_to_windows_codepage(utf16_str));
+  std::wstring utf16 = utf8_to_utf16(utf8_string);
+  return utf16_to_windows_codepage(utf16);
 }
 
 /**
- * @brief Converts UTF8 const char* to Windows codepage std::string
+ * @brief Converts from UTF-8 to Windows codepage
  *
- * If fails, returns empty string, std::string("")
- *
- * @param utf8_string for UTF8 const char* to convert
+ * @param utf8_string for UTF-8 string to convert
  * @return std::string for the converted Windows codepage string
  *
- * @sa <a target ="_blank"
- * href="002-conversion_8cpp_source.html">002-conversion.cpp</a>
  */
 inline std::string utf8_to_windows_codepage(const char *utf8_string) {
   if (utf8_string == nullptr)
     return "";
-  if (strlen(utf8_string) < 1)
-    return "";
-
-  return utf8_to_windows_codepage(std::string(utf8_string));
+  std::wstring utf16 = utf8_to_utf16(utf8_string);
+  return utf16_to_windows_codepage(utf16);
 }
 
 /**
- * @brief Converts from source to target.
+ * @brief Template function for source to target conversion
  *
- * @tparam TargetCharType
- * @tparam SourceCharType
- * @param str
- * @return decltype(auto)
+ * @param str for source string to convert
+ * @return decltype(auto) for the converted target string
+ *
  */
 template <typename TargetCharType, typename SourceCharType>
 decltype(auto) source_to_target(const std::basic_string<SourceCharType> &str) {
-  if constexpr (std::is_same_v<TargetCharType, SourceCharType>) {
+  if constexpr (std::is_same_v<TargetCharType, char> &&
+                std::is_same_v<SourceCharType, wchar_t>)
+    return wstring_to_string(str);
+  else if constexpr (std::is_same_v<TargetCharType, wchar_t> &&
+                     std::is_same_v<SourceCharType, char>)
+    return string_to_wstring(str);
+  else
     return str;
-  } else if constexpr (std::is_same_v<SourceCharType, char>) {
-    return utf8_to_utf16(str);
-  } else {
-    return utf16_to_utf8(str);
-  }
 }
 
+/**
+ * @brief Smart encode function for char*
+ *
+ * @param arg for char* to encode
+ * @return std::string for the encoded string
+ *
+ */
 inline std::string smart_encode(const char *arg) {
-#ifdef _MSC_VER
-  return {arg};
-#else
-  return conversion::utf8_to_windows_codepage(arg);
-#endif
+  if (arg == nullptr)
+    return "";
+  return std::string(arg);
 }
 
+/**
+ * @brief Smart encode function for wchar_t*
+ *
+ * @param arg for wchar_t* to encode
+ * @return std::string for the encoded string
+ *
+ */
 inline std::string smart_encode(const wchar_t *arg) {
-  return conversion::utf16_to_windows_codepage(arg);
+  if (arg == nullptr)
+    return "";
+  return wstring_to_string(std::wstring(arg));
 }
-
-} // end of namespace conversion
-
-} // end of namespace cpg
+} // namespace conversion
+} // namespace cpg
 
 #endif // end of file _CPG_CONVERSION_HPP
